ff902f1aff0ba684320176144045d90ebc816117
noxca <ahmadrizkidaffaa@gmail.com>
2023-10-12 16:22:15 +0700

    Revising forms

--- application/forms.py
+++ application/forms.py
@@ -4,19 +4,20 @@ from wtforms import StringField, PasswordField, SubmitField, EmailField, TextAre
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
+    username = StringField("Username", validators=[DataRequired(), Length(min = 4)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min = 8)])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password", message = "Password must match!")])
     submit = SubmitField("Sign Up")
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min = 1)])
+    username = StringField("Username", validators=[DataRequired(), Length(min = 4)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min = 8)])
     submit = SubmitField("Login")
 
 class EditProfile(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min = 1)])
+    username = StringField("Username")
+    password = PasswordField("Password")
+    profile_pic = StringField("Bio")
     bio = StringField("Bio")
     submit = SubmitField("Update Profile")
 
